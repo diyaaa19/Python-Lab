d43e84e075d31bb0be5b5f53bc8059b8e73ec1e5
diyaaa19 <151644762+diyaaa19@users.noreply.github.com>
2024-09-09 23:55:18 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter a number: "))
-fact=1
-if(n<0):
-    print("Enter a positive number")
-if(n==0):
-    print("Factorial is 1")
-for i in range(1,n+1):
-    fact=fact*i
-print("Factorial of",n,"is",fact)
