dc2a8d66d2346ca778e8fa11482aaac2901c707f
Diya Jain <151644762+diyaaa19@users.noreply.github.com>
2024-09-16 10:13:04 +0530

    Update armstrong.py

--- lab-2/armstrong.py
+++ lab-2/armstrong.py
@@ -1,8 +1,9 @@
 a=int(input("Enter a number : "))
+n=len(str(a))
 sum=0
 temp=a
 while(temp>0):
-    sum=sum+((temp%10)**3)
+    sum=sum+((temp%10)**n)
     temp=temp//10
 if(sum==a):
     print("armstrong number")
