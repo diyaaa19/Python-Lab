6039ca1de1c547ace9b79229011d35cb12712575
Diya Jain <151644762+diyaaa19@users.noreply.github.com>
2024-09-16 10:12:15 +0530

    Update armstrong_1_1000.py

--- lab-3/armstrong_1_1000.py
+++ lab-3/armstrong_1_1000.py
@@ -1,8 +1,9 @@
 for i in range(1,1001):
+    n=len(str(i))
     sum=0
     temp=i
     while(temp>0):
-        sum=sum+((temp%10)**3)
+        sum=sum+((temp%10)**n)
         temp=temp//10
     if(sum==i):
         print(i)
